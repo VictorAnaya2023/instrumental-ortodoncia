d94e258605174c1fe448092d86c15754ffc5189d
chikipack <victorgotzilla@hotmail.com>
2024-01-20 17:33:36 -0800

    videos card

--- instrumental/src/app/components/videos/videos.component.ts
+++ instrumental/src/app/components/videos/videos.component.ts
@@ -1,12 +1,123 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-videos',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './videos.component.html',
   styleUrl: './videos.component.css'
 })
 export class VideosComponent {
+  dummy_Data = [
+    {
+      "nombre": "Video 1",
+      "video": "https://www.youtube.com/watch?v=video1",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 1"
+    },
+    {
+      "nombre": "Video 2",
+      "video": "https://www.youtube.com/watch?v=video2",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 2"
+    },
+    {
+      "nombre": "Video 3",
+      "video": "https://www.youtube.com/watch?v=video3",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 3"
+    },
+    {
+      "nombre": "Video 4",
+      "video": "https://www.youtube.com/watch?v=video4",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 4"
+    },
+    {
+      "nombre": "Video 5",
+      "video": "https://www.youtube.com/watch?v=video5",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 5"
+    },
+    {
+      "nombre": "Video 6",
+      "video": "https://www.youtube.com/watch?v=video6",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 6"
+    },
+    {
+      "nombre": "Video 7",
+      "video": "https://www.youtube.com/watch?v=video7",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 7"
+    },
+    {
+      "nombre": "Video 8",
+      "video": "https://www.youtube.com/watch?v=video8",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 8"
+    },
+    {
+      "nombre": "Video 9",
+      "video": "https://www.youtube.com/watch?v=video9",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 9"
+    },
+    {
+      "nombre": "Video 10",
+      "video": "https://www.youtube.com/watch?v=video9",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 9"
+    },
+    {
+      "nombre": "Video 11",
+      "video": "https://www.youtube.com/watch?v=video9",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 9"
+    },
+    {
+      "nombre": "Video 12",
+      "video": "https://www.youtube.com/watch?v=video9",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 9"
+    },
+    {
+      "nombre": "Video 13",
+      "video": "https://www.youtube.com/watch?v=video9",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 9"
+    },
+    {
+      "nombre": "Video 14",
+      "video": "https://www.youtube.com/watch?v=video9",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 9"
+    },
+    {
+      "nombre": "Video 15",
+      "video": "https://www.youtube.com/watch?v=video9",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 9"
+    },
+    {
+      "nombre": "Video 16",
+      "video": "https://www.youtube.com/watch?v=video9",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 9"
+    },
+    {
+      "nombre": "Video 17",
+      "video": "https://www.youtube.com/watch?v=video9",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 9"
+    },
+    {
+      "nombre": "Video 18",
+      "video": "https://www.youtube.com/watch?v=video10",
+      "imagen": "https://ddazul.com/productos/20191111111930-32fd022c748bcd7471b833d6186846b0.jpg",
+      "descripcion": "Descripción del Video 10"
+    }
+  ]
 
 }
