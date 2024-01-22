d538218af4778e34bdce58807ea4a8a218923f0a
VictorAnaya2023 <148127887+VictorAnaya2023@users.noreply.github.com>
2024-01-22 15:40:09 -0800

    login component

--- instrumental/src/app/auth/login/login.component.ts
+++ instrumental/src/app/auth/login/login.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  hide = true;
 
+  constructor(private authService: AuthService, private router: Router){}
+
+  onSubmit(): void {
+    console.log('webos');
+    // Realiza la lógica de autenticación
+    this.authService.isAuth();
+    console.log('webos3');
+    // Redirige al usuario a la ruta deseada (por ejemplo, '/home')
+    this.router.navigate(['/home']);
+  }
 }
