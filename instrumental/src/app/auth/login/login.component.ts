48422d4c623cffa7c73348ef86dc527a7d7dc0ba
VictorAnaya2023 <148127887+VictorAnaya2023@users.noreply.github.com>
2024-02-06 13:19:03 -0800

    login service

--- instrumental/src/app/auth/login/login.component.ts
+++ instrumental/src/app/auth/login/login.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDivider} from '@angular/material/divider';
+import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+
+import {CookieService} from 'ngx-cookie-service';
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -17,22 +22,39 @@ import { Router } from '@angular/router';
     MatButtonModule,
     MatIconModule,
     MatDivider,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username!: string;
+  password!: string;
   hide = true;
 
-  constructor(private authService: AuthService, private router: Router){}
+  constructor(private authService: AuthService, private router: Router, private cookieService: CookieService){}
 
   login(): void {
+    try{
+      this.authService.login(this.username, this.password).subscribe({
+        next:(res:any)=>{
+          this.cookieService.set('accessToken', res.data.token, undefined, '/', undefined, true, 'Strict');
+        },
+        error:(resErr)=>{console.log(resErr)},
+        complete:()=>{
+          this.router.navigate(['/admin-panel']);
+        }
+      })
+    }
+    catch(error){
+
+    }
     // Realiza la lógica de autenticación
-    this.authService.login();
+    // this.authService.login();
     // console.log('webos3');
     // Redirige al usuario a la ruta deseada (por ejemplo, '/home')
-    this.router.navigate(['/admin-panel']);
-    this.router.navigate(['/home']);
+    // this.router.navigate(['/admin-panel']);
+    // this.router.navigate(['/home']);
   }
 
   register(): void {
--- instrumental/src/app/services/auth.service.ts
+++ instrumental/src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -6,14 +8,27 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   token = ''
   logged:boolean=false;
-  constructor() { }
+  constructor(private http: HttpClient, private cookieService: CookieService) { }
 
-  login(){
-    this.logged = true;
+  login(username:string, password:string){
+    const body = new URLSearchParams()
+    body.set('email', username);
+    body.set('password', password);
+    const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+    return this.http.post('https://api.instrumentaldeortodoncia.com/api/login', body.toString(), {headers})
+  }
+
+  checkAccessTokenCookie() {
+    const accessTokenExists = this.cookieService.check('accessToken');
+    if (accessTokenExists) {
+      return true
+    } else {
+      return false
+    }
   }
 
   isAuth(){
-    if(this.logged){
+    if(this.checkAccessTokenCookie()){
       return true;
     }else{
       return false;
